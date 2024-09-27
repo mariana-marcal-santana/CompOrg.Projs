00e1f50c72abdbd4dc9df53fd29880ebcc8f4e05
Carlota Domingos <92582049+carlota-domingos@users.noreply.github.com>
2024-09-27 19:24:10 +0100

    Update SimpleProgram.c L2_1W

--- P1.CacheHierarchy/L2_1W/SimpleProgram.c
+++ P1.CacheHierarchy/L2_1W/SimpleProgram.c
@@ -7,6 +7,20 @@ int main() {
 
   int clock1, value;
 
+  resetTime();
+  initCache();
+
+  //try to read from an empty cache
+  double avg = 0;
+  int last = 0;
+  for(int i = 0; i < DRAM_SIZE/4; i+=WORD_SIZE) {
+    read(i, (unsigned char *)(&value));
+    clock1 = getTime();
+    avg += clock1-last;
+    last = clock1;
+  }  
+  printf("Average time to read from an empty cache: %f\n", avg/(DRAM_SIZE/4));
+  
   for(int n = 1; n <= DRAM_SIZE/4; n*=WORD_SIZE) {
 
     resetTime();
@@ -25,7 +39,7 @@ int main() {
       clock1 = getTime();
       printf("Read; Address %d; Value %d; Time %d\n", i, value, clock1);
     }  
-
+    printf("\n Number of words:%d | Time:%d\n",(n-1)/WORD_SIZE+1,clock1);
   }
 
   printf("\nRandom accesses\n");
@@ -46,6 +60,35 @@ int main() {
       printf("Write; Address %d; Value %d; Time %d\n", address, address, clock1);
     }
   }
-  
+  printf("time:%d\n",clock1); 
+
+  //random acesses to the cache
+ 
+  for(int n = 1; n <=(1<<16); n*=WORD_SIZE){
+    resetTime();
+    initCache();
+    avg = 0;
+    last = 0;
+    int reads = 0;
+    for(int i = 0; i < n; i+=WORD_SIZE) {
+      int address = rand() % (DRAM_SIZE/4);
+      address = address - address % WORD_SIZE;
+      int mode = rand() % 2;
+      if (mode == MODE_READ) {
+        read(address, (unsigned char *)(&value));
+        clock1 = getTime();
+        
+      }
+      else {
+        write(address, (unsigned char *)(&address));
+        clock1 = getTime();
+      }
+      avg += clock1-last;
+      reads++;
+      last = clock1;
+    }
+    printf("Average time to read from a cache with %d words: %f\n", n, avg/reads);
+    
+  }
   return 0;
 }
