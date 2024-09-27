c692bd0a31ec4a733200012f71643268a5068ded
Carlota Domingos <92582049+carlota-domingos@users.noreply.github.com>
2024-09-27 19:23:34 +0100

    Update SimpleProgram.c L1

--- P1.CacheHierarchy/L1/SimpleProgram.c
+++ P1.CacheHierarchy/L1/SimpleProgram.c
@@ -7,27 +7,49 @@ int main() {
 
   int clock1, value;
 
-  for(int n = 1; n <= DRAM_SIZE/4; n*=WORD_SIZE) {
+  resetTime();
+  initCache();
 
+  //try to read from an empty cache
+  double avg = 0;
+  int last = 0;
+  for(int i = 0; i < DRAM_SIZE/4; i+=WORD_SIZE) {
+    read(i, (unsigned char *)(&value));
+    clock1 = getTime();
+    avg += clock1-last;
+    last = clock1;
+  }  
+  printf("Average time to read from an empty cache: %f\n", avg/(DRAM_SIZE/4));
+  
+
+
+  // Test the cache with sequential accesses
+  
+  for(int n = 1; n <= DRAM_SIZE/4; n*=WORD_SIZE) {
+    
     resetTime();
     initCache();
-
+    
     printf("\nNumber of words: %d\n", (n-1)/WORD_SIZE + 1);
     
     for(int i = 0; i < n; i+=WORD_SIZE) {
       write(i, (unsigned char *)(&i));
       clock1 = getTime();
       printf("Write; Address %d; Value %d; Time %d\n", i, i, clock1);
+      
     }
 
     for(int i = 0; i < n; i+=WORD_SIZE) {
       read(i, (unsigned char *)(&value));
       clock1 = getTime();
       printf("Read; Address %d; Value %d; Time %d\n", i, value, clock1);
-    }  
 
+    }  
+    printf("\n Number of words:%d | Time:%d\n",(n-1)/WORD_SIZE+1,clock1);
+     
   }
 
+ 
   printf("\nRandom accesses\n");
 
   // Do random accesses to the cache
@@ -47,5 +69,33 @@ int main() {
     }
   }
   
+  //different size random acesses to the cache
+ 
+  for(int n = 1; n <=(1<<16); n*=WORD_SIZE){
+    resetTime();
+    initCache();
+    avg = 0;
+    last = 0;
+    int reads = 0;
+    for(int i = 0; i < n; i+=WORD_SIZE) {
+      int address = rand() % (DRAM_SIZE/4);
+      address = address - address % WORD_SIZE;
+      int mode = rand() % 2;
+      if (mode == MODE_READ) {
+        read(address, (unsigned char *)(&value));
+        clock1 = getTime();
+        
+      }
+      else {
+        write(address, (unsigned char *)(&address));
+        clock1 = getTime();
+      }
+      avg += clock1-last;
+      reads++;
+      last = clock1;
+    }
+    printf("Average time to read from a cache with %d words: %f\n", n, avg/reads);
+    
+  }
   return 0;
 }
